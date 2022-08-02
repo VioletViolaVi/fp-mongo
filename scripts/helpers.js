b80d3a02ea26cca052094fbaacd0c993389871e0
vivian <vivian.v18723@gmail.com>
2022-08-02 18:14:14 +0100

    practice w/ mongodb

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,10 +1,19 @@
 db = connect("mongodb://localhost:27017/shelter");
 
+// .sort(), .limit(), .count(), etc are mongoDB NOT Js!
 
-function numberOfDogsByBreed(b){
-    return db.dogs.find({ breed: { $eq: b }}).count();
+
+function numberOfDogsByBreed(b) {
+  return db.dogs.find({ breed: { $eq: b } }).count();
+}
+function ageOfDogs(ageParam) {
+  return db.dogs.find({ age: { $eq: ageParam } });
 }
 
-function findOldestDog(){
-    return db.dogs.find().sort({ "age" : -1 }).limit(1);
+function findOldestDog() {
+  return db.dogs.find().sort({ age: -1 }).limit(1);
+}
+
+function findYoungestDog() {
+  return db.dogs.find().sort({ age: 1 }).limit(1);
 }
